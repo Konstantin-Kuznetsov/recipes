9bffdc43aa2719dd16d6a2a85ab37f5d65ceadbe
KonstantinKuznetsov <kuznetsovka@tutu.ru>
2021-10-21 09:49:32 +0300

    DAO methods return type fixed

--- app/src/main/java/com/example/recipes/data/database/dao/RecipesDao.kt
+++ app/src/main/java/com/example/recipes/data/database/dao/RecipesDao.kt
@@ -19,10 +19,10 @@ interface RecipesDao {
      * Replace recipes every time we reload data from API to ensure that we have the actual version of a recipe
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertRecipes(recipes: List<RecipeEntity>): Long
+    fun insertRecipes(recipes: List<RecipeEntity>)
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insertFavInfo(favInfo: List<FavouriteRecipeEntity>): Long
+    fun insertFavInfo(favInfo: List<FavouriteRecipeEntity>)
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
     fun updateFavInfo(favInfo: FavouriteRecipeEntity)
