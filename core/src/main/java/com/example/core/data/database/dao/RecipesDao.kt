5f69294740ac22390b57408261c3ec87c3bec878
KonstantinKuznetsov <kuznetsovka@tutu.ru>
2021-11-04 14:15:34 +0300

    DAO for recipes DB extended with transaction for upserting recipes

--- core/src/main/java/com/example/core/data/database/dao/RecipesDao.kt
+++ core/src/main/java/com/example/core/data/database/dao/RecipesDao.kt
@@ -15,12 +15,27 @@ interface RecipesDao {
     @Query("SELECT * FROM recipes")
     suspend fun getAllRecipes(): List<FullRecipeInfo>
 
+    @Transaction
+    open fun insertOrUpdateRecipesWithFavInfo(updatedRecipes: List<RecipeEntity>) {
+        insertRecipes(updatedRecipes)
+        updatedRecipes.map {
+            FavouriteRecipeEntity(
+                recipeId = it.recipeId,
+                isFavourite = false, // default value for new recipes
+                lastUpdateMillis = System.currentTimeMillis()
+            )
+        }.let(this::insertFavInfo)
+    }
+
     /*
      * Replace recipes every time we reload data from API to ensure that we have the actual version of a recipe
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertRecipes(recipes: List<RecipeEntity>)
 
+    /*
+     * Save favourite info for existing fields and create for new recipes
+     */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertFavInfo(favInfo: List<FavouriteRecipeEntity>)
 
